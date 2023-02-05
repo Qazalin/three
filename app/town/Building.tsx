8205be67baf9d830a544ed5beef0181d11f02486
qazal <qazal.saamani@gmail.com>
2023-02-05 16:30:12 +0000

    500 shades of grass

--- app/town/Building.tsx
+++ app/town/Building.tsx
@@ -1,7 +1,13 @@
-import { Geometry, Base, Subtraction, Addition } from "@react-three/csg";
+import {
+  Geometry,
+  Base,
+  Subtraction,
+  Addition,
+  SubtractionProps,
+} from "@react-three/csg";
 import { MeshProps } from "@react-three/fiber";
 import React, { useRef } from "react";
-import { blue } from "tailwindcss/colors";
+import { blue, emerald, green, stone } from "tailwindcss/colors";
 import { Setup } from "./Setup";
 
 type Dimension = {
@@ -20,6 +26,38 @@ const HOUSE_DIMENSIONS = {
   width: 1.5,
   height: 2,
 };
+const colors = {
+  grass: "#5C7C41",
+};
+
+const grassShades = {
+  100: "#FFFFFF",
+  200: "#F4F8F2",
+  300: "#EAF1E4",
+  400: "#DFEAD7",
+  500: "#D5E3CA",
+  600: "#CADCBC",
+  700: "#C0D5AF",
+  800: "#B5CEA1",
+  900: "#ABC794",
+  A100: "#A0C087",
+  A200: "#96B979",
+  A400: "#8BB26C",
+  A700: "#81AB5F",
+  B100: "#77A054",
+  B200: "#6D834D",
+  B300: "#637744",
+  B400: "#596A3D",
+  B500: "#4F5E36",
+  B600: "#45522F",
+  B700: "#3B4628",
+  B800: "#314021",
+  B900: "#273419",
+  C100: "#1E2A12",
+  C200: "#151F0B",
+  C300: "#0B1304",
+  C400: "#000000",
+};
 
 function getArgs(dims: Dimension): [number, number, number] {
   return [dims.depth, dims.width, dims.height];
@@ -31,7 +69,7 @@ export default function SanFrancisco() {
     <Setup>
       <House rotation={[0, 0, Math.PI / 2]} position={[0, 1, 0]} />
       <Floor />
-      <Tree position={[-1, 0.1, 3]} rotation={[0, 0, Math.PI / 2]} />
+      <Tree position={[-1, 0.159, 3.2]} rotation={[0, 0, Math.PI / 2]} />
     </Setup>
   );
 }
@@ -39,8 +77,22 @@ export default function SanFrancisco() {
 const Floor: CustomMesh = (props) => {
   return (
     <mesh receiveShadow castShadow {...props}>
-      <boxGeometry args={getArgs(FLOOR_DIMENSIONS)} />
-      <meshLambertMaterial color="#6A6A6C" />
+      <Geometry computeVertexNormals useGroups>
+        <Base>
+          <boxGeometry args={getArgs(FLOOR_DIMENSIONS)} />
+          <meshLambertMaterial color={stone[900]} />
+        </Base>
+        <Addition position={[-1.4, 0.06, 3.4]} name="grass">
+          <boxGeometry args={[1, 0.001, 0.8]} />
+          <meshBasicMaterial color={grassShades.B200} />
+        </Addition>
+        <Addition position={[-1.6, 0.06, 3.4]}>
+          <sphereGeometry
+            args={[0.2, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]}
+          />
+          <meshBasicMaterial color={grassShades.B500} />
+        </Addition>
+      </Geometry>
     </mesh>
   );
 };
@@ -146,7 +198,7 @@ function BalconyArea() {
   );
 }
 
-const Window = (props) => (
+const Window = (props: SubtractionProps) => (
   <Subtraction name="window" {...props}>
     <boxGeometry args={[0.2, 2, 0.4]} />
     <meshLambertMaterial color="#6A6B6E" />
@@ -157,9 +209,9 @@ const Tree: CustomMesh = (props) => {
   return (
     <>
       <mesh castShadow {...props}>
-        <mesh position={[0.32, 0, 0]}>
-          <boxGeometry args={[0.4, 0.3, 0.3]} />
-          <meshBasicMaterial color="#5C7C41" />
+        <mesh position={[0.24, 0, 0]}>
+          <boxGeometry args={[0.35, 0.3, 0.3]} />
+          <meshBasicMaterial color={grassShades.B400} />
         </mesh>
         <mesh>
           <boxGeometry args={[0.2, 0.1, 0.1]} />
